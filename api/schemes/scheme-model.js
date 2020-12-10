9d39151c76022ed9db40c225ec2fbac724f43c9c
tarmes <traarmes@gmail.com>
2020-12-09 20:44:07 -0500

    findSteps()

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -9,5 +9,23 @@ module.exports = {
       return db('schemes')
          .where('id', id)
          .first()
+   },
+   findSteps(id) {
+      return db('steps')
+         .join('schemes', 'steps.scheme_id', 'schemes.id')
+         .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+         .where('scheme_id', id)
+         .orderBy('steps.step_number', 'asc')
    }
 }
+
+// select 
+// 	steps.id,
+// 	schemes.scheme_name,
+// 	steps.step_number,
+// 	steps.instructions
+// from steps
+// join schemes
+// 	on steps.scheme_id = schemes.id
+// where scheme_id = 2
+// order by steps.step_number asc;
