a8c6fa096f10e2c04521c65b4a8a869d49f91108
tarmes <traarmes@gmail.com>
2020-12-09 20:25:38 -0500

    find() findById()

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,13 @@
 // scheme-model
+const db = require('../../data/db-config')
+
+module.exports = {
+   find() {
+      return db('schemes')
+   },
+   findById(id) {
+      return db('schemes')
+         .where('id', id)
+         .first()
+   }
+}
